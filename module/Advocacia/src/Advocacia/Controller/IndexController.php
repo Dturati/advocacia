f9f3021a6cdaedcf83c4081b50fc73c6cc831199
David Turati <davidturati@gmail.com>
2015-12-25 17:17:16 -0300

    commit 1

---
+++ module/Advocacia/src/Advocacia/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Advocacia\Controller;
+
+use Zend\View\Model\ViewModel;
+use Zend\Mvc\Controller\AbstractActionController;
+
+
+class IndexController extends AbstractActionController {
+    
+    public function indexAction() {
+        return new ViewModel();
+    }
+    
+}
